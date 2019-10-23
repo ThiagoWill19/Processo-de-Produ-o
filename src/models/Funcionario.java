cc629fc9745a1386f22ece3869b11a7ec546ebf2
ThiagoWill <thiagopompeu19@gmail.com>
2019-10-22 23:48:50 -0200

    getters and setters and builder added to model classes

--- src/models/Funcionario.java
+++ src/models/Funcionario.java
@@ -7,6 +7,7 @@ package models;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import models.enums.Funcao;
 
 /**
@@ -19,5 +20,74 @@ public class Funcionario {
     private String nome;
     private Funcao funcao;
     private List<Servico> servicos =  new ArrayList<>();
+    
+    public Funcionario(){
+        
+    }
+
+    public Funcionario(Integer id, String nome, Funcao funcao) {
+        this.id = id;
+        this.nome = nome;
+        this.funcao = funcao;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Funcao getFuncao() {
+        return funcao;
+    }
+
+    public void setFuncao(Funcao funcao) {
+        this.funcao = funcao;
+    }
+
+    public List<Servico> getServicos() {
+        return servicos;
+    }
+
+    public void setServicos(List<Servico> servicos) {
+        this.servicos = servicos;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 53 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Funcionario other = (Funcionario) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
     
 }
--- src/models/OrdemServico.java
+++ src/models/OrdemServico.java
@@ -7,6 +7,7 @@ package models;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  *
@@ -21,5 +22,101 @@ public class OrdemServico {
     private boolean status;
     private Integer idUltimoAcesso;
     private List<FuncionarioRelacionado> fr = new ArrayList<>();
+    
+    private OrdemServico(){
+        
+    }
+
+    public OrdemServico(Integer id, String empresa, String cliente, String processo, boolean status, Integer idUltimoAcesso) {
+        this.id = id;
+        this.empresa = empresa;
+        this.cliente = cliente;
+        this.processo = processo;
+        this.status = status;
+        this.idUltimoAcesso = idUltimoAcesso;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getProcesso() {
+        return processo;
+    }
+
+    public void setProcesso(String processo) {
+        this.processo = processo;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public Integer getIdUltimoAcesso() {
+        return idUltimoAcesso;
+    }
+
+    public void setIdUltimoAcesso(Integer idUltimoAcesso) {
+        this.idUltimoAcesso = idUltimoAcesso;
+    }
+
+    public List<FuncionarioRelacionado> getFr() {
+        return fr;
+    }
+
+    public void setFr(List<FuncionarioRelacionado> fr) {
+        this.fr = fr;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 79 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final OrdemServico other = (OrdemServico) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
     
 }
